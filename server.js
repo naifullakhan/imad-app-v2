4e62d3885f7325bf40ffcd3748f3e99a55f09c20
naifullakhan <naifkingkhan786@gmail.com>
2017-02-26 11:23:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get('/counter',function(req,res){
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-app.get('/ui/style,main.js',function (req,res){
+app.get('/ui/style.main.js',function (req,res){
    res.senfile(path.join(_dirname, 'ui', 'main.js'));
    
 });
