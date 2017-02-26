e756e0ec0f3ccd3dfbc656d80ba6cb61fd8c86aa
naifullakhan <naifkingkhan786@gmail.com>
2017-02-26 11:18:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,14 @@
-console.log('Loaded!');
-king
-khan
+//counter code 
+var button = document.getElementById('counter');
+var counter =0;
+button.click = function() {
+    //make a requestb tonthe cpounter endpoint
+    
+    //capture the response and store it in a variable
+    
+    //render the variable in the correct span
+    
+    counter = counter + 1;
+    var span =document.getelmentbyid('count');
+    span.innerHTML =counter.toString();
+};
