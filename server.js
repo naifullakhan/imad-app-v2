d033e47416964deeb121037d4b7c901eb3c1e26d
naifullakhan <naifkingkhan786@gmail.com>
2017-02-26 12:04:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.listen(8080, function () {
 var counter=0;
 app.get('/counter',function(req,res){
     counter=counter+1;
-    res,send(counter.tostring());
+    res,send(counter.toString());
 
 
 });
